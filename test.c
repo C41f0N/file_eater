414cb5c1084fb31baeee72cf62042f7bb3cadcf6
Sarim Ahmed <C41f0N@protonmail.com>
2023-11-25 15:56:39 +0500

    Made a snake minigame

--- test.c
+++ test.c
@@ -1,75 +1,94 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <unistd.h>
-#include <ncurses.h>
-#define WIDTH 50
-#define HEIGHT 10
+#define GAMESCREEN_WIDTH 50
+#define GAMESCREEN_HEIGHT 10
 
-struct Snake
+struct SnakeGame
 {
     int vel_x;
     int vel_y;
+    int food_x;
+    int food_y;
+    int score;
+    int running;
+
     int length;
     int top;
-    int body[WIDTH * HEIGHT][2];
+    int snakeBody[GAMESCREEN_WIDTH * GAMESCREEN_HEIGHT][2];
 };
 
-int foodPos[2];
-struct Snake snake;
 int temp;
+struct SnakeGame snakeGame;
 
-char snakeChar = 'X';
+char inputCh;
+char snakeChar = '0';
 char backgroundChar = ' ';
-char borderChar = '0';
-char foodChar = 'D';
+char borderChar = 'X';
+char foodChar = 'a';
+
+void putFood()
+{
+    snakeGame.food_x = (rand() % (GAMESCREEN_WIDTH - 2)) + 1;
+    snakeGame.food_y = (rand() % (GAMESCREEN_HEIGHT - 2)) + 1;
+}
 
 void initialize()
 {
     // Initialize all values of the array
-    for (int i = 0; i < (HEIGHT * WIDTH); i++)
+    for (int i = 0; i < (GAMESCREEN_HEIGHT * GAMESCREEN_WIDTH); i++)
     {
-        snake.body[i][0] = -1;
-        snake.body[i][1] = -1;
+        snakeGame.snakeBody[i][0] = -1;
+        snakeGame.snakeBody[i][1] = -1;
     }
 
-    snake.body[0][0] = (int)WIDTH / 2;
-    snake.body[0][1] = (int)HEIGHT / 2;
+    snakeGame.score = 0;
 
-    snake.body[0][0] = (int)(WIDTH / 2) - 1;
-    snake.body[0][1] = (int)HEIGHT / 2;
+    snakeGame.snakeBody[0][0] = (int)GAMESCREEN_WIDTH / 2;
+    snakeGame.snakeBody[0][1] = (int)GAMESCREEN_HEIGHT / 2;
 
-    snake.body[0][0] = (int)(WIDTH / 2) - 2;
-    snake.body[0][1] = (int)HEIGHT / 2;
+    snakeGame.snakeBody[0][0] = (int)(GAMESCREEN_WIDTH / 2) - 1;
+    snakeGame.snakeBody[0][1] = (int)GAMESCREEN_HEIGHT / 2;
 
-    snake.length = 3;
+    snakeGame.snakeBody[0][0] = (int)(GAMESCREEN_WIDTH / 2) - 2;
+    snakeGame.snakeBody[0][1] = (int)GAMESCREEN_HEIGHT / 2;
 
-    snake.vel_x = 1;
-    snake.vel_y = 0;
+    snakeGame.length = 3;
+
+    putFood();
+
+    snakeGame.vel_x = 1;
+    snakeGame.vel_y = 0;
 }
 
 int checkGameOver()
 {
-    for (int i = 0; i < (WIDTH * HEIGHT); i++)
+    if ((snakeGame.snakeBody[0][0] + snakeGame.vel_x) < (GAMESCREEN_WIDTH - 1) &&
+        (snakeGame.snakeBody[0][0] + snakeGame.vel_x) > 0 &&
+        (snakeGame.snakeBody[0][1] + snakeGame.vel_y) < (GAMESCREEN_HEIGHT - 1) &&
+        (snakeGame.snakeBody[0][1] + snakeGame.vel_y) > 0)
     {
-        if (snake.body[i][0] == 0 || snake.body[i][0] == WIDTH - 1 || snake.body[i][1] == 0 || snake.body[i][1] == HEIGHT - 1)
-        {
-            return 1;
-        }
+        return 0;
     }
-    return 0;
+    else
+    {
+        return 1;
+    }
 }
 
 void gameOver()
 {
-    exit(0);
+    snakeGame.running = 0;
 }
 
 int snakeIsHere(int x, int y)
 {
-    for (int i = 0; i < (WIDTH * HEIGHT); i++)
+    for (int i = 0; i < (GAMESCREEN_WIDTH * GAMESCREEN_HEIGHT); i++)
     {
         // check x and y
-        if (x == snake.body[i][0] && y == snake.body[i][1])
+        if (x == snakeGame.snakeBody[i][0] && y == snakeGame.snakeBody[i][1])
         {
             return 1;
         }
@@ -77,18 +96,9 @@ int snakeIsHere(int x, int y)
     return 0;
 }
 
-void putFood()
-{
-    // foodPos[0] = (rand() % (WIDTH - 2)) + 1;
-    // foodPos[1] = (rand() % (HEIGHT - 2)) + 1;
-
-    foodPos[0] = snake.body[0][0] + 6;
-    foodPos[1] = snake.body[0][1];
-}
-
 int hasEatenFood()
 {
-    if (snakeIsHere(foodPos[0], foodPos[1]))
+    if (snakeIsHere(snakeGame.food_x, snakeGame.food_y))
     {
         return 1;
     }
@@ -107,46 +117,47 @@ void update()
 
     if (hasEatenFood())
     {
-        snake.length++;
+        snakeGame.length++;
+        snakeGame.score++;
         putFood();
     }
 
     // Shift all places to right
-    for (int i = (HEIGHT * WIDTH) - 1; i >= 0; i--)
+    for (int i = (GAMESCREEN_HEIGHT * GAMESCREEN_WIDTH) - 1; i >= 0; i--)
     {
-        snake.body[i + 1][0] = snake.body[i][0];
-        snake.body[i + 1][1] = snake.body[i][1];
+        snakeGame.snakeBody[i + 1][0] = snakeGame.snakeBody[i][0];
+        snakeGame.snakeBody[i + 1][1] = snakeGame.snakeBody[i][1];
     }
 
-    snake.body[0][0] += snake.vel_x;
-    snake.body[0][1] += snake.vel_y;
+    snakeGame.snakeBody[0][0] += snakeGame.vel_x;
+    snakeGame.snakeBody[0][1] += snakeGame.vel_y;
 
-    snake.top++;
+    snakeGame.top++;
 
     // Do a cleanup
-    while (snake.top + 1 > snake.length)
+    while (snakeGame.top + 1 > snakeGame.length)
     {
-        snake.body[snake.top][0] = -1;
-        snake.body[snake.top][1] = -1;
-        snake.top--;
+        snakeGame.snakeBody[snakeGame.top][0] = -1;
+        snakeGame.snakeBody[snakeGame.top][1] = -1;
+        snakeGame.top--;
     }
 }
 
 void render()
 {
-    for (int j = 0; j < HEIGHT; j++)
+    for (int j = 0; j < GAMESCREEN_HEIGHT; j++)
     {
-        for (int i = 0; i < WIDTH; i++)
+        for (int i = 0; i < GAMESCREEN_WIDTH; i++)
         {
             if (snakeIsHere(i, j))
             {
                 printf("%c", snakeChar);
             }
-            else if (i == 0 || i == WIDTH - 1 || j == 0 || j == HEIGHT - 1)
+            else if (i == 0 || i == GAMESCREEN_WIDTH - 1 || j == 0 || j == GAMESCREEN_HEIGHT - 1)
             {
                 printf("%c", borderChar);
             }
-            else if (i == foodPos[0] && j == foodPos[1])
+            else if (i == snakeGame.food_x && j == snakeGame.food_y)
             {
                 printf("%c", foodChar);
             }
@@ -157,30 +168,63 @@ void render()
         }
         printf("\n");
     }
+    printf("SCORE: %d", snakeGame.score);
 }
 
 void updateVelocity()
 {
-    if ()
+    if (_kbhit())
     {
-        printf("\nGot it");
+        inputCh = getch();
+        if ((inputCh == 'w' || inputCh == 'W') && snakeGame.vel_y != 1)
+        {
+            snakeGame.vel_x = 0;
+            snakeGame.vel_y = -1;
+        }
+        else if ((inputCh == 'a' || inputCh == 'A') && snakeGame.vel_x != 1)
+        {
+            snakeGame.vel_x = -1;
+            snakeGame.vel_y = 0;
+        }
+        else if ((inputCh == 's' || inputCh == 'S') && snakeGame.vel_y != -1)
+        {
+            snakeGame.vel_x = 0;
+            snakeGame.vel_y = 1;
+        }
+        else if ((inputCh == 'd' || inputCh == 'D') && snakeGame.vel_x != -1)
+        {
+            snakeGame.vel_x = 1;
+            snakeGame.vel_y = 0;
+        }
     }
 }
 
 void main()
 {
-    system("clear");
-
-    initialize();
-
-    putFood();
+    snakeGame.running = 1;
 
     while (1)
     {
-        update();
-        render();
-        usleep(500000);
-        updateVelocity();
-        system("clear");
+        // SnakeGame Starts
+        system("cls");
+        initialize();
+
+        // SnakeGame Runs
+        while (snakeGame.running)
+        {
+            update();
+            render();
+            Sleep(250);
+            updateVelocity();
+            fflush(stdin);
+            system("cls");
+        }
+
+        // SnakeGame Over
+        printf("\nGAME OVER.");
+        printf("\nPress any key to try again.");
+        getch();
+        snakeGame.running = 1;
+        fflush(stdin);
     }
 }
